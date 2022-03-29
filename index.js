1c36c2b67b0cbfd63fa533b94466407765fa829c
Gareth Dawson <gwdawson.work@gmail.com>
2022-03-29 11:34:54 +0100

    Add GET /api/users

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { getTopics } = require('./controllers/topics.controller');
 const { getArticleById, patchArticleById } = require('./controllers/articles.controller');
+const { getUsers } = require('./controllers/users.controller');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
-
+app.get('/api/users', getUsers);
 app.get('/api/articles/:article_id', getArticleById);
 app.patch('/api/articles/:article_id', patchArticleById);
 
