9717f23dd9616f2dcc149b2fd7588cfe9584303f
Gareth Dawson <gwdawson.work@gmail.com>
2022-03-29 10:47:54 +0100

    Add PATCH /api/articles/article_id

--- models/articles.model.js
+++ models/articles.model.js
@@ -4,3 +4,11 @@ exports.fetchArticleById = async (article_id) => {
   const { rows } = await db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id]);
   return rows[0];
 };
+
+exports.updateArticleById = async (article_id, inc_votes) => {
+  const { rows } = await db.query('UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *', [
+    inc_votes,
+    article_id,
+  ]);
+  return rows[0];
+};
