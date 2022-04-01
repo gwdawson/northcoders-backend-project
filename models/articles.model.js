75a446fe596f918bd0e2820e65a0058ae7d5cf27
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-01 17:08:05 +0100

    Remove pg-format require

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,4 @@
 const db = require('../db/connection');
-const format = require('pg-format');
 
 exports.fetchArticleById = async (article_id) => {
   const { rows } = await db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id]);
